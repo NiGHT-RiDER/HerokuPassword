5e62d46f01d12cc00619a6a759ad1f986e5da19d
NiGHT-RiDER <NiGHTRiDERgithub@tfwno.gf>
2016-12-01 14:58:09 +0100

    dbhelper resolution bug

--- helpers/database.js
+++ helpers/database.js
@@ -6,8 +6,8 @@
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk(process.env.MONGOLAB_URI);
-var pass = db.get('passwords');
-var users = db.get('users');
+var pas = db.get('passwords');
+var user = db.get('users');
 
 /*
  * -------------
@@ -21,6 +21,5 @@ var users = db.get('users');
  * -- EXPORTS
  * -----------
  */
-exports.db = db;
-exports.pass = pass;
-exports.users = users;
+exports.pass = pas;
+exports.users = user;
